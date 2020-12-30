160ba84f6096fcc28a6e24acc530cc6a1ac257dc
Mrudul23 <74598719+Mrudul23@users.noreply.github.com>
2020-12-30 16:02:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,24 +6,24 @@ const Body = Matter.Body;
 var engine, world;
 
 function setup() {
-	createCanvas(2300, 800);
+	createCanvas(1535, 530);
    
 
 	engine = Engine.create();
 	world = engine.world;
 
-	paper = new Paper(100, 500, 70);
+	paper = new Paper(100, 300, 50);
 	
 
-  ground = new Ground(1150, 750, 2300, 90);
+  ground = new Ground(768, 500, 2300, 60);
   
-  leftSide = new Dustbin(1800, 550, 50, 320);
-  bottom = new Dustbin(2000, 685, 350, 50);
-  rightSide = new Dustbin(2200, 550, 50, 320);
-  wall = new Dustbin(2300,380,10,750)
+  leftSide = new Dustbin(1205, 370, 37, 200);
+  bottom = new Dustbin(1335, 451, 222, 37);
+  rightSide = new Dustbin(1465, 370, 37, 200);
+  wall = new Dustbin(1535,380,10,750)
   wall2 = new Dustbin(1150,0,2300,10)
   wall3 = new Dustbin(0,380,10,750)
-  wall4 = new Dustbin(1150,800,2300,10)
+  wall4 = new Dustbin(767.5,530,2300,10)
 
 	Engine.run(engine);
   
@@ -47,14 +47,14 @@ function draw() {
   leftSide.display();
   bottom.display();
   rightSide.display(); 
-  
+
   drawSprites(); 
 
   text("trash",paper.x,paper.y)
 
-  textSize(30)
-  text("► Press right arrow to bounce towards right side | ► Your goal is to put the trash into the dust bin",10,740)
-  text("► Press left arrow to bounce towards left side     | ► the blue color ball is the trash                                                                                                       Dustbin ↑",10,780)
+  textSize(20)
+  text("► Press right arrow to bounce towards right side | ► Your goal is to put the trash into the dust bin",10,490)
+  text("► Press left arrow to bounce towards left side     | ► the blue color ball is the trash                                                                                                       Dustbin ↑",10,520)
   
   
 
@@ -63,9 +63,9 @@ function draw() {
 
 function keyPressed(){
   if(keyCode === RIGHT_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, {x:2000, y: -1300})
+    Matter.Body.applyForce(paper.body, paper.body.position, {x:800, y: -500})
   }
   if(keyCode === LEFT_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, {x:-2000, y: -1300})
+    Matter.Body.applyForce(paper.body, paper.body.position, {x:-800, y: -500})
   }
 }
